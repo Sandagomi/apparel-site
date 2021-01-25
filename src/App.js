69fb364b47662db01ea443d3ab6c96a12eadf3be
Sandagomi <sandagomi@leafycode.com>
2021-01-25 16:31:56 +0530

    feat: initiate header fundamentals. :lion: :heart:

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.components';
 import {Switch,Route} from 'react-router-dom';
 import ShopPage from './pages/shoppage/shoppage.components'
+import Header from './components/header/header.components';
 
 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
-    <Route exact path='/' component={HomePage}/>
-    <Route path='/shop' component={ShopPage}/>
+         <Route exact path='/' component={HomePage}/>
+          <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );

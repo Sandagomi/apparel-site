032f631159e6ef6e54606dc025ef639c13e645ba
sandagomi <sandagomitk@gmail.com>
2021-02-10 14:26:19 +0530

    feat: complete User Types Actions :lion: :fireworks:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React,{useEffect} from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.components';
-import {Switch,Route} from 'react-router-dom';
+import {Switch,Route,Redirect} from 'react-router-dom';
 import {connect} from 'react-redux'
 import {setCurrentUser} from "./redux/user/user.action";
 import ShopPage from './pages/shoppage/shoppage.components'
@@ -11,7 +11,7 @@ import { auth,createUserProfileDocument } from './firebase/firebase.utils';
 
 
 
-const App = ({setCurrentUser}) =>  {
+const App = ({setCurrentUser,currentUser}) =>  {
 
     useEffect(()=> {
 
@@ -39,14 +39,17 @@ const App = ({setCurrentUser}) =>  {
       <Switch>
          <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={SignInSignUpPage}/>
+          <Route exact path='/signin' render={()=>currentUser ? (<Redirect to='/' />) : (<SignInSignUpPage/>) }/>
       </Switch>
     </div>
   );
 }
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
 
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);

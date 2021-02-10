95b6b7d1d5b284432cd1051ae64ae06e01162b95
sandagomi <sandagomitk@gmail.com>
2021-02-10 15:23:13 +0530

    feat: complete cart DropDown component :fire: :ocean:

--- src/components/header/header.components.jsx
+++ src/components/header/header.components.jsx
@@ -4,6 +4,8 @@ import {connect} from 'react-redux'
 import {auth} from '../../firebase/firebase.utils'
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/4.2 crown.svg'
+import CartIcon from "../cart-icon/cart-icon.components";
+import CartDropdown from "../cart-dropdown/cart-dropdown.components";
 
 
 const Header = ({currentUser}) => {
@@ -24,7 +26,9 @@ const Header = ({currentUser}) => {
                     currentUser ? <div className="option" onClick={()=>auth.signOut()}>SIGN OUT</div>
                         : <Link className="option" to="/signin">SIGN IN</Link>
                 }
+                <CartIcon/>
             </div>
+          <CartDropdown/>
         </div>
     )
 }
